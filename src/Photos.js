5272ec685482eeefcdfc8a656d7f208c9ca2a2fb
casnafu <camiferreir@gmail.com>
2021-11-06 23:15:40 +0100

    add photos component

---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Photos(props) {
+    if (props.data) {
+        return (
+            <div className="Photos bg-white border-white shadow-sm rounded-3 my-4 p-4">
+                <div className="row">
+                    {props.data.map((photo, index) => {
+                        return (
+                            <div className="col-md-4">
+                                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                                    <img src={photo.src.landscape} key={index} className="img-fluid m-2 border-3 rounded" alt="" />
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    } else {
+        return null;
+    }
+}
--- src/Search.js
+++ src/Search.js
@@ -1,40 +1,75 @@
 import React, { useState } from "react";
-import Definition from "./Definition";
 import axios from "axios";
+import Definition from "./Definition";
+import Photos from "./Photos";
 
 function Search() {
-    const [keyword, setKeyword] = useState(null);
+    const [keyword, setKeyword] = useState("hello");
     const [definition, setDefinition] = useState(null);
+    const [photos, setPhotos] = useState(null);
+    const [ready, setReady] = useState(false);
 
     function getResponse(response) {
         setDefinition(response.data[0]);
     }
 
+    function getImg(response) {
+        setPhotos(response.data.photos);
+    }
+
+    function search() {
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(url).then(getResponse);
+
+        const pexels = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        const pexelsAPI = "563492ad6f917000010000013184da26ffc54dabac22f537cfd1cc8c";
+        const headers = { Authorization: `Bearer ${pexelsAPI}` };
+
+        axios.get(pexels, { headers: headers }).then(getImg)
+    }
+
     function trackSearch(event) {
         event.preventDefault();
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-
-        axios.get(url).then(getResponse);
+        search()
     }
 
     function trackKeyword(event) {
         setKeyword(event.target.value);
     }
 
-    return (
-        <div className="Search">
-            <form className="container-fluid" onSubmit={trackSearch}>
-                <input
-                    className="form-control search-input rounded-pill p-3 my-4"
-                    placeholder="Try somenthing like... your favorite thing"
-                    type="search"
-                    autoFocus="on"
-                    onChange={trackKeyword}
-                />
-            </form>
-            <Definition data={definition} />
-        </div>
-    )
+    function load() {
+        setReady(true);
+        search();
+    }
+
+
+    if (ready) {
+        return (
+            <div className="Search">
+                <form className="container-fluid justify-content-center" onSubmit={trackSearch}>
+                    <input
+                        className="form-control search-input rounded-pill p-3 my-4"
+                        placeholder="Try smth like... your favorite thing"
+                        type="search"
+                        autoFocus="on"
+                        onChange={trackKeyword}
+                    />
+                </form>
+                <Definition data={definition} />
+                <Photos data={photos} />
+            </div>
+        )
+    } else {
+        load();
+        return (
+            <div class="text-center">
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default Search;
