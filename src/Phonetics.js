5b6d36d84f9200483e102f8c310bfac4ae013d25
casnafu <camiferreir@gmail.com>
2021-11-06 16:31:09 +0100

    audio button styled n' function added

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default function Phonetics(props) {
+    const audio = props.data.audio;
+    function listenAudio() {
+        new Audio(audio).play();
+    }
     return (
-        <section>
-            <a href={props.data.audio} target="_blank" rel="noreferrer"><i className="fas fa-volume-up"></i></a>
-            {props.data.text}
-        </section>
+        < section >
+            <button className="px-1 bg-white border rounded-pill" onClick={listenAudio}><i className="fas fa-volume-up text-primary"></i></button>
+            <span className="px-2">{props.data.text}</span>
+        </section >
     )
 }
